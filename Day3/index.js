ef0e9c3abfb365f429eea96a50df9cc23999792a
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2022-12-10 23:11:07 +0530

    Update index.js

--- Day3/index.js
+++ Day3/index.js
@@ -1,19 +1,15 @@
+// To create express server 
+// we need 2 lines
+
 const express = require("express");
-const routes = require("./routes/authRoutes");
 const app = express();
 
-// accept json
-app.use(express.json());
-// accept body
-app.use(express.urlencoded({ extended: true }));
-// use the html
-app.use(express.static("public"))
-
-  
-app.use("/api/v1", routes);
-
+// Specify port of our express server 
 const PORT = 1337;
 
+// provide callback when successfull run on this port 
+// to check our express server is running
+// call back is if everything ok till here call this function
 
 app.listen(PORT, () => {
     console.log('App is running at port=', PORT);
