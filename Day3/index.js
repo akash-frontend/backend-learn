c2aed0c001d6d279064f75933e4755f4e9cf5de4
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2022-12-10 23:05:53 +0530

    Add files via upload

---
+++ Day3/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = require("./routes/authRoutes");
+const app = express();
+
+// accept json
+app.use(express.json());
+// accept body
+app.use(express.urlencoded({ extended: true }));
+// use the html
+app.use(express.static("public"))
+
+  
+app.use("/api/v1", routes);
+
+const PORT = 1337;
+
+
+app.listen(PORT, () => {
+    console.log('App is running at port=', PORT);
+  });
